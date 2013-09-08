9a73e9fed58164cb9e5ccab5a097e1fe92f2ad8d
MiltonQuirino <miltonquirino.br@gmail.com>
2013-09-08 20:34:34 -0300

    add eclipse

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@ class User < ActiveRecord::Base
 	validates :email, :full_name, :location, :presence => true
 
 	validates_confirmation_of :password
-#	validates_length_of :bio,minimun => 30, allow_blank => false
 	validates_length_of :bio, :minimum => 30, :allow_blank => false
 
-
+ #
 	validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 	validates_uniqueness_of :email
 end
